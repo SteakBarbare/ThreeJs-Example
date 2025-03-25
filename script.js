4dd6d622ee5c240da9704e269d92f6462bfd9a92
Steak Barbare <cortodufour@gmail.com>
2025-03-25 03:35:39 +0100

    Add: Pointer Lock Camera, Raytracing & Random Block Generation

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { FirstPersonControls } from "three/addons/controls/FirstPersonControls.js";
+import { PointerLockControls } from "three/addons/controls/PointerLockControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import gsap from "gsap";
 import GUI from "lil-gui";
@@ -27,6 +28,7 @@ const camPos = {
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
+  antialias: true,
   canvas: canvas
 });
 renderer.setSize(sizes.width, sizes.height);
@@ -144,7 +146,7 @@ scene.add(sphere);
 sphere.position.set(-3, 0, 0);
 
 const floor = new THREE.Mesh(
-  new THREE.PlaneGeometry(20, 20),
+  new THREE.PlaneGeometry(2000, 2000, 100, 100),
   sciFiFloorMaterial
 );
 floor.rotation.x = -Math.PI * 0.5;
@@ -161,9 +163,11 @@ gltfLoader.load("Models/Skull/scene.gltf", gltf => {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = camPos.z;
 scene.add(camera);
-const controls = new FirstPersonControls(camera, canvas);
+// const controls = new FirstPersonControls(camera, canvas);
 camera.lookAt(mesh.position);
 camera.position.set(camPos.x, camPos.y, camPos.z);
+
+const controls = new PointerLockControls(camera, document.body);
 
 renderer.render(scene, camera);
 
@@ -179,7 +183,7 @@ const tick = () => {
   sphere.rotation.x += 0.01;
   sphere.rotation.y += 0.01;
 
-  controls.update(0.1);
+  // controls.update(0.1);
 
   // Update sizes
   sizes.width = window.innerWidth;
@@ -195,10 +199,250 @@ const tick = () => {
   // Update Render
   renderer.render(scene, camera);
 
-  console.log(skull);
+  // Movements
+  const time = performance.now();
+
+  if (controls.isLocked) {
+    raycaster.ray.origin.copy(controls.object.position);
+    raycaster.ray.origin.y -= 10;
+
+    const intersections = raycaster.intersectObjects(objects, false);
+
+    const onObject = intersections.length > 0;
+
+    const delta = (time - prevTime) / 1000;
+
+    velocity.x -= velocity.x * 10.0 * delta;
+    velocity.z -= velocity.z * 10.0 * delta;
+
+    velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
+
+    direction.z = Number(moveForward) - Number(moveBackward);
+    direction.x = Number(moveRight) - Number(moveLeft);
+    direction.normalize(); // this ensures consistent movements in all directions
+
+    if (moveForward || moveBackward) velocity.z -= direction.z * 400.0 * delta;
+    if (moveLeft || moveRight) velocity.x -= direction.x * 400.0 * delta;
+
+    if (onObject === true) {
+      velocity.y = Math.max(0, velocity.y);
+      canJump = true;
+    }
+
+    controls.moveRight(-velocity.x * delta);
+    controls.moveForward(-velocity.z * delta);
+
+    controls.object.position.y += velocity.y * delta; // new behavior
+
+    if (controls.object.position.y < 10) {
+      velocity.y = 0;
+      controls.object.position.y = 10;
+
+      canJump = true;
+    }
+  }
+
+  prevTime = time;
+
   if (skull != undefined) {
     skull.position.y += 0.01;
   }
 };
+
+const objects = [];
+let raycaster;
+
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+let canJump = false;
+
+let prevTime = performance.now();
+const velocity = new THREE.Vector3();
+const direction = new THREE.Vector3();
+const vertex = new THREE.Vector3();
+const color = new THREE.Color();
+
+init();
+
+function init() {
+  scene.background = new THREE.Color(0xffffff);
+  scene.fog = new THREE.Fog(0xffffff, 0, 750);
+
+  const light = new THREE.HemisphereLight(0xeeeeff, 0x777788, 2.5);
+  light.position.set(0.5, 1, 0.75);
+  scene.add(light);
+
+  const blocker = document.getElementById("blocker");
+  const instructions = document.getElementById("instructions");
+
+  instructions.addEventListener("click", function () {
+    controls.lock();
+  });
+
+  controls.addEventListener("lock", function () {
+    instructions.style.display = "none";
+    blocker.style.display = "none";
+  });
+
+  controls.addEventListener("unlock", function () {
+    blocker.style.display = "block";
+    instructions.style.display = "";
+  });
+
+  scene.add(controls.object);
+
+  const onKeyDown = function (event) {
+    switch (event.code) {
+      case "ArrowUp":
+      case "KeyW":
+        moveForward = true;
+        break;
+
+      case "ArrowLeft":
+      case "KeyA":
+        moveLeft = true;
+        break;
+
+      case "ArrowDown":
+      case "KeyS":
+        moveBackward = true;
+        break;
+
+      case "ArrowRight":
+      case "KeyD":
+        moveRight = true;
+        break;
+
+      case "Space":
+        if (canJump === true) velocity.y += 350;
+        canJump = false;
+        break;
+    }
+  };
+
+  const onKeyUp = function (event) {
+    switch (event.code) {
+      case "ArrowUp":
+      case "KeyW":
+        moveForward = false;
+        break;
+
+      case "ArrowLeft":
+      case "KeyA":
+        moveLeft = false;
+        break;
+
+      case "ArrowDown":
+      case "KeyS":
+        moveBackward = false;
+        break;
+
+      case "ArrowRight":
+      case "KeyD":
+        moveRight = false;
+        break;
+    }
+  };
+
+  document.addEventListener("keydown", onKeyDown);
+  document.addEventListener("keyup", onKeyUp);
+
+  raycaster = new THREE.Raycaster(
+    new THREE.Vector3(),
+    new THREE.Vector3(0, -1, 0),
+    0,
+    10
+  );
+
+  // floor
+
+  // let floorGeometry = new THREE.PlaneGeometry(2000, 2000, 100, 100);
+  // floorGeometry.rotateX(-Math.PI / 2);
+
+  // // vertex displacement
+
+  // let position = floorGeometry.attributes.position;
+
+  // for (let i = 0, l = position.count; i < l; i++) {
+  //   vertex.fromBufferAttribute(position, i);
+
+  //   vertex.x += Math.random() * 20 - 10;
+  //   vertex.y += Math.random() * 2;
+  //   vertex.z += Math.random() * 20 - 10;
+
+  //   position.setXYZ(i, vertex.x, vertex.y, vertex.z);
+  // }
+
+  // floorGeometry = floorGeometry.toNonIndexed(); // ensure each face has unique vertices
+
+  // position = floorGeometry.attributes.position;
+  // const colorsFloor = [];
+
+  // for (let i = 0, l = position.count; i < l; i++) {
+  //   color.setHSL(
+  //     Math.random() * 0.3 + 0.5,
+  //     0.75,
+  //     Math.random() * 0.25 + 0.75,
+  //     THREE.SRGBColorSpace
+  //   );
+  //   colorsFloor.push(color.r, color.g, color.b);
+  // }
+
+  // floorGeometry.setAttribute(
+  //   "color",
+  //   new THREE.Float32BufferAttribute(colorsFloor, 3)
+  // );
+
+  // const floorMaterial = new THREE.MeshBasicMaterial({ vertexColors: true });
+
+  // const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+  // scene.add(floor);
+
+  // objects
+
+  const boxGeometry = new THREE.BoxGeometry(20, 20, 20).toNonIndexed();
+
+  let position = boxGeometry.attributes.position;
+  const colorsBox = [];
+
+  for (let i = 0, l = position.count; i < l; i++) {
+    color.setHSL(
+      Math.random() * 0.3 + 0.5,
+      0.75,
+      Math.random() * 0.25 + 0.75,
+      THREE.SRGBColorSpace
+    );
+    colorsBox.push(color.r, color.g, color.b);
+  }
+
+  boxGeometry.setAttribute(
+    "color",
+    new THREE.Float32BufferAttribute(colorsBox, 3)
+  );
+
+  for (let i = 0; i < 500; i++) {
+    const boxMaterial = new THREE.MeshPhongMaterial({
+      specular: 0xffffff,
+      flatShading: true,
+      vertexColors: true
+    });
+    boxMaterial.color.setHSL(
+      Math.random() * 0.2 + 0.5,
+      0.75,
+      Math.random() * 0.25 + 0.75,
+      THREE.SRGBColorSpace
+    );
+
+    const box = new THREE.Mesh(boxGeometry, boxMaterial);
+    box.position.x = Math.floor(Math.random() * 20 - 10) * 20;
+    box.position.y = Math.floor(Math.random() * 20) * 20 + 10;
+    box.position.z = Math.floor(Math.random() * 20 - 10) * 20;
+
+    scene.add(box);
+    objects.push(box);
+  }
+}
 
 tick();
